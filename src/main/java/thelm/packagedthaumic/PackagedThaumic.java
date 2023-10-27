25e3b29e53229e511984cc6daa24b0d4759297e8
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-10-27 22:51:21 +0800

    version

--- src/main/java/thelm/packagedthaumic/PackagedThaumic.java
+++ src/main/java/thelm/packagedthaumic/PackagedThaumic.java
@@ -26,7 +26,7 @@ public class PackagedThaumic {
 
 	public static final String MOD_ID = "packagedthaumic";
 	public static final String NAME = "PackagedThaumic";
-	public static final String VERSION = "1.12.2-1.0.1.2";
+	public static final String VERSION = "1.12.2-1.0.2.3";
 	public static final String DEPENDENCIES = "required-after:packagedauto@[1.12.2-1.0.10,);required-after:thaumcraft;";
 	public static final String GUI_FACTORY = "thelm.packagedthaumic.client.gui.GuiPackagedThaumicConfigFactory";
 	public static final CreativeTabs CREATIVE_TAB = new CreativeTabs("packagedthaumic") {
